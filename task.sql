1603f137026d971c6327ec6ffb8597f4db32c1be
Yuriy.Khilko <yurakhilko@gmail.com>
2025-01-13 13:56:14 +0200

    added fixes after review

--- task.sql
+++ task.sql
@@ -2,13 +2,14 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders ( CustomerID, Date)
+VALUES ( 1, '2025-01-13');
 
 -- Start the transaction 
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
-INSERT INTO Orders ( CustomerID, Date)
-VALUES ( 1, '2025-01-13');
+
 update Products p
 set p.WarehouseAmount = 41
 where p.id = '1';
