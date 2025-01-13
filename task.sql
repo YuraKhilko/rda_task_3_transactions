6903cd192354a8e6b4dbefd34a97e4b7030065ea
Yuriy.Khilko <yurakhilko@gmail.com>
2025-01-13 13:51:44 +0200

    added dml

--- task.sql
+++ task.sql
@@ -7,5 +7,13 @@ USE ShopDB;
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
+INSERT INTO Orders ( CustomerID, Date)
+VALUES ( 1, '2025-01-13');
+update Products p
+set p.WarehouseAmount = 41
+where p.id = '1';
+
+INSERT INTO OrderItems ( OrderID, ProductID,Count)
+VALUES ( 1, 1,1);
 
 COMMIT; 
